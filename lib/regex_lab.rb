18431287c0a706b8d0953e8a303bef87f092b61f
Ben Hogan <j.benhogan@gmail.com>
2020-06-28 20:40:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiouAEIOU]/)
-    true 
-  else
-    false 
-  end
+  word.match(/^[aeiouAEIOU]/) ? true : false 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\b^[A-Z]*[.!?]$/) ? true : false 
+  text.match(^[A-Z].+[\.!?]$) ? true : false 
 end
 
 def valid_phone_number?(phone)
