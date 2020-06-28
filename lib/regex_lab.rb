acd5fadc42b9f796e2354f9d86780aa6f3f783f1
Ben Hogan <j.benhogan@gmail.com>
2020-06-28 20:25:29 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,13 +11,13 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.length == 5 
+  text.scan(//) 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  
+  text.scan(//)
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(//)
 end
